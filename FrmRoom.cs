15d0cc605572ffb86854ab9b82288c41ba880660
dungmytan <114470197+dungmytan@users.noreply.github.com>
2022-09-29 18:47:41 +0700

    Add files via upload

---
+++ FrmRoom.cs
@@ -0,0 +1,15 @@
+namespace CleanCodeTuan01
+{
+    public partial class FrmRoom : Form
+    {
+        public FrmRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_status_light_on_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
